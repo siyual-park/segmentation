7bc1c5740ee43999cb26f73497b0bf2f3dc4c4d7
ara.bak <siyual.bak@gmail.com>
2021-09-22 01:43:58 +0900

    feat: normalize loss

--- src/loss/__init__.py
+++ src/loss/__init__.py
@@ -16,4 +16,4 @@ This should be differentiable.
 
     loss = 1 - ((2. * intersection + smooth) / (iflat.sum(dim=1) + tflat.sum(dim=1) + smooth))
 
-    return loss.sum()
+    return loss.sum() / batch_size
