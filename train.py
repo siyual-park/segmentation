9080fbf91f9dfe65df2d8c4d44c6fbd838a2e39a
ara.bak <siyual.bak@gmail.com>
2021-09-21 21:20:12 +0900

    change deep and channels

--- train.py
+++ train.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--image_size', type=int, default=128)
     parser.add_argument('--batch_size', type=int, default=4)
-    parser.add_argument('--deep', type=int, default=4)
+    parser.add_argument('--deep', type=int, default=2)
     parser.add_argument('--dropout_prob', type=float, default=0.4)
 
     args = parser.parse_args()
