7c8eb765ab0fd2484d7924966ca048e93dca5174
ara.bak <siyual.bak@gmail.com>
2021-09-22 14:01:47 +0900

    change deep to 8

--- detect.py
+++ detect.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     parser.add_argument('--image_size', type=int, default=128)
     parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--channels', type=int, default=128)
-    parser.add_argument('--deep', type=int, default=16)
+    parser.add_argument('--deep', type=int, default=8)
 
     args = parser.parse_args()
 
