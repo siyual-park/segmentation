bb7a80e1ecf05c518103db5004e282a342000b4a
ara.bak <siyual.bak@gmail.com>
2021-09-22 10:56:21 +0900

    feat: change smooth

--- src/loss/__init__.py
+++ src/loss/__init__.py
@@ -1,11 +1,9 @@
-def dice_loss(pred, target):
+def dice_loss(pred, target, smooth: float = 1e-6):
     """This definition generalize to real valued pred and target vector.
 This should be differentiable.
     pred: tensor with first dimension as batch
     target: tensor with first dimension as batch
     """
-
-    smooth = 1.
 
     # have to use contiguous since they may from a torch.view op
     batch_size = target.size(0)
